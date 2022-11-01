28794dff52e36bfcd010b6c35a072a248210fff4
hareemfatima10 <41821583+hareemfatima10@users.noreply.github.com>
2022-11-01 12:36:38 +0400

    Update datasets.py

--- code/datasets.py
+++ code/datasets.py
@@ -9,7 +9,7 @@ from torchvision import transforms
 
 from PIL import Image, ImageFilter
 
-import HeadPose.code.utils
+import utils
 
 def get_list_from_filenames(file_path):
     # input:    relative path to .txt file with file names
